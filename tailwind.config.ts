af9fa873a8093d463010992a87fa5d3359c84051
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-24 16:30:06 -0500

    chore(config): add custom colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "custom-bg-primary": "var(--bg-primary)",
+        "custom-text-primary": "var(--text-primary)",
+        "custom-text-secondary": "var(--text-secondary)",
+        "custom-bg-button": "var(--bg-button)",
+        "custom-text-button": "var(--text-button)",
+        "custom-bg-card": "var(--bg-card)",
+        "custom-border-card": "var(--border-card)",
+        "custom-bg-footer": "var(--bg-footer)",
+        "custom-text-footer": "var(--text-footer)",
+        "custom-bg-header": "var(--bg-header)",
+        "custom-text-header": "var(--text-header)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
