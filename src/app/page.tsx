c713b465b53cb1f00ef591fc712631722036c106
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-24 16:41:36 -0500

    feat(navigation): add redirect to catalog in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import {redirect} from "next/navigation";
+
 export default async function Home() {
+  redirect ("/catalog");
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 font-bold text-4xl text-blue-600'>
       Hello, world!
