93eb24914c92538ee7efffec79a49d11632243e7
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-25 18:08:43 -0500

    style(catalog): improve layout and spacing

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,6 @@
 import { GamesList } from "@/components/pages/catalog/GamesList";
 import { GenreFilter } from "@/components/pages/catalog/GenreFilter";
 import { getGames } from "@/services/getGames";
-import Image from "next/image";
 
 export default async function Catalog(props: {searchParams?: {genre?: string, page?: string}}) {
 
@@ -12,16 +11,16 @@ export default async function Catalog(props: {searchParams?: {genre?: string, pa
 
 	const {games, availableFilters, totalPages, currentPage} = await getGames({genre, page});
 	return (
-		<div className="container-custom py-8">
-			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-10 tracking-[0.4px]">
+		<div className="container-custom py-8 xs:py-10 md:py-12 flex flex-col gap-12">
+			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left tracking-[0.4px]">
 				Top Sellers
 			</h2>
 			{/* Genre Filter Select */}
 			<GenreFilter />
 
-    	{/* Product Grid */}
+			{/* Product Grid */}
 			<GamesList games={games} />
-			
+
 		</div>
 	);
 }
