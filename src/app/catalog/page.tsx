a4df443084916cf951adbb0b4dc54a48a9110d16
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-25 12:41:00 -0500

    refactor(catalog): rename GenreFilterSelect component to GenreFilter

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,18 +1,17 @@
 import { GamesList } from "@/components/GamesList";
-import { GenreFilterSelect } from "@/components/GenreFilterSelect";
+import { GenreFilter } from "@/components/GenreFilter";
 import { getGames } from "@/services/getGames";
 import Image from "next/image";
 
 export default async function Catalog() {
-
-	const {games, availableFilters, totalPages, currentPage} = await getGames();
+	const {games, availableFilters, totalPages, currentPage} = await getGames({});
 	return (
 		<div className="container-custom py-8">
 			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-10 tracking-[0.4px]">
 				Top Sellers
 			</h2>
 			{/* Genre Filter Select */}
-			<GenreFilterSelect availableFilters={availableFilters} />
+			<GenreFilter availableFilters={availableFilters} />
 
     	{/* Product Grid */}
 			<GamesList games={games} />
