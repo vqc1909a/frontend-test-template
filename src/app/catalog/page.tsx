3cd0642ebb023b7e9506ea9066e6b221e0d72b46
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-25 03:43:17 -0500

    style(catalog): adjust margin for top sellers heading

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -8,7 +8,7 @@ export default async function Catalog() {
 	const {games, availableFilters, totalPages, currentPage} = await getGames();
 	return (
 		<div className="container-custom py-8">
-			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-8 tracking-[0.4px]">
+			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-10 tracking-[0.4px]">
 				Top Sellers
 			</h2>
 			{/* Genre Filter Select */}
