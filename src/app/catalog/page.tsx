35c56245acb9b0240e0ec09e879f59b55af0a48c
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-24 19:30:16 -0500

    fix(catalog): adjust heading styles for responsive design

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,7 @@
 export default function Catalog() {
   return (
 		<div className="container-custom py-8">
-			<h2 className="uppercase sm:normal-case text-2xl leading-7 sm:text-4xl sm:leading-10 font-bold text-left mb-8 tracking-[0.4px]">
+			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-8 tracking-[0.4px]">
 				Top Sellers
 			</h2>
 		</div>
