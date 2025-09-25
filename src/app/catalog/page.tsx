281a6d9e0579eee3e25e0845b8923f729c2616a4
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-24 19:29:17 -0500

    feat(catalog): update Catalog component to display Top Sellers heading

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,9 @@
 export default function Catalog() {
   return (
-    <div>
-      <h1>Catalog</h1>
-    </div>
-  );
+		<div className="container-custom py-8">
+			<h2 className="uppercase sm:normal-case text-2xl leading-7 sm:text-4xl sm:leading-10 font-bold text-left mb-8 tracking-[0.4px]">
+				Top Sellers
+			</h2>
+		</div>
+	);
 }
