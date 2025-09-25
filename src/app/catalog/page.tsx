fcdcec8a4b460f38af20a22e433d82df491810b2
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-25 01:24:38 -0500

    feat(catalog): integrate GamesList component

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,10 +1,11 @@
+import { GamesList } from "@/components/GamesList";
 import { GenreFilterSelect } from "@/components/GenreFilterSelect";
 import { getGames } from "@/services/getGames";
+import Image from "next/image";
 
 export default async function Catalog() {
 
 	const {games, availableFilters, totalPages, currentPage} = await getGames();
-
 	return (
 		<div className="container-custom py-8">
 			<h2 className="uppercase md:normal-case text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left mb-8 tracking-[0.4px]">
@@ -13,6 +14,9 @@ export default async function Catalog() {
 			{/* Genre Filter Select */}
 			<GenreFilterSelect availableFilters={availableFilters} />
 
+    	{/* Product Grid */}
+			<GamesList games={games} />
+			
 		</div>
 	);
 }
