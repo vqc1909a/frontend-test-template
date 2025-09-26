ac165591c2efcfb29a95d52a915e4851fe021018
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 02:32:56 -0500

    feat(catalog): add conditional rendering for empty games list in GamesList

--- src/components/pages/catalog/GamesList.tsx
+++ src/components/pages/catalog/GamesList.tsx
@@ -9,6 +9,11 @@ interface GamesListProps {
 export const GamesList = async ({genre, page}: GamesListProps) => {
 	const {games} = await getGames({genre, page});
 	
+	if(!games.length) return (
+		<div className="text-xl md:text-2xl font-bold text-custom-text-primary text-start">
+			No Games Found
+		</div>
+	);
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-12">
 			{games.map((game) => (
