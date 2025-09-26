d37f394fa0d72f44f13e7e85ff6a038868be12db
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 01:07:05 -0500

    feat(catalog): implement dynamic pagination URL generation

--- src/components/pages/catalog/GamesButton.tsx
+++ src/components/pages/catalog/GamesButton.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { ReadonlyURLSearchParams, usePathname, useSearchParams } from "next/navigation";
+
 
 interface GamesButtonProps {
   hasMorePages: boolean;
+	page: number;
 }
 
-export const GamesButton = ({hasMorePages}: GamesButtonProps) => {
+const createPageURL = (
+	page: number,
+	pathname: string,
+	searchParams: ReadonlyURLSearchParams
+) => {
+	const params = new URLSearchParams(searchParams);
+	params.set("page", String(page));
+	return `${pathname}?${params.toString()}`;
+};
+
+export const GamesButton = ({hasMorePages, page}: GamesButtonProps) => {
+  const pathname = usePathname();
+	const searchParams = useSearchParams();
+
 	if (!hasMorePages) return;
 	return (
 		<div className="flex justify-start items-center">
-			<Link href="/" className="btn-primary">
+			<Link href={createPageURL(page + 1, pathname, searchParams)} className="btn-primary">
 				See More
 			</Link>
 		</div>
