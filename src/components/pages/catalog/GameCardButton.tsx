87307ce461f1547aed69e6cbd27acde780afba7a
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 15:38:36 -0500

    refactor(catalog): update GameCardButton to use cart state and improve cart management

--- src/components/pages/catalog/GameCardButton.tsx
+++ src/components/pages/catalog/GameCardButton.tsx
@@ -1,47 +1,72 @@
 'use client'
 
+import { Game } from "@/services/getGames";
 import { useEffect, useState } from "react";
 
 interface GameCardButtonProps {
-  gameId: string;
+	game: Game;
 }
 
-export const GameCardButton = ({gameId}: GameCardButtonProps) => {
+interface CartItem {
+	product: Game;
+	quantity: number;
+}
+
+export const GameCardButton = ({game}: GameCardButtonProps) => {
+
+	const [cart, setCart] = useState<{cartItems: CartItem[], totalPrice: number}>({
+		cartItems: [],
+		totalPrice: 0,
+	});
 
 	const [productAdded, setProductAdded] = useState(false);
 
   const addToCart = () => {
-		const cartItems = JSON.parse(localStorage.getItem("cart") || "[]");
+		const cartItems = JSON.parse(localStorage.getItem("cart") || '{"cartItems": [], "totalPrice": 0}')?.cartItems || [];
 
 		const existingItem = cartItems.find(
-			(item: any) => item.id === gameId
+			(item: any) => item.product.id === game.id
 		);
 
 		if (existingItem) {
 			existingItem.quantity += 1;
 		} else {
-			cartItems.push({id: gameId, quantity: 1});
+			cartItems.push({product: game, quantity: 1});
 		}
-
-		localStorage.setItem("cart", JSON.stringify(cartItems));
+		const totalPrice = cartItems.reduce((acc: number, value: CartItem) => acc + value.product.price * value.quantity, 0);
+		localStorage.setItem("cart", JSON.stringify({cartItems, totalPrice}));
+		setCart({
+			cartItems,
+			totalPrice
+		})
 		setProductAdded(true);
 	};
 
 	const removeFromCart = () => {
-		const cartItems = JSON.parse(localStorage.getItem("cart") || "[]");
+		const cartItems = JSON.parse(localStorage.getItem("cart") || '{"cartItems": [], "totalPrice": 0}')?.cartItems || [];
 		
 		const updatedCartItems = cartItems.filter(
-			(item: any) => item.id !== gameId
+			(item: any) => item.product.id !== game.id
 		);
-		localStorage.setItem("cart", JSON.stringify(updatedCartItems));
+		const totalPrice = updatedCartItems.reduce((acc: number, value: CartItem) => acc + value.product.price * value.quantity, 0);
+		localStorage.setItem("cart", JSON.stringify({cartItems: updatedCartItems, totalPrice}));
+		setCart({
+			cartItems: updatedCartItems,
+			totalPrice
+		})
 		setProductAdded(false);
 	};
 
 	useEffect(() => {
-		const cartItems = JSON.parse(localStorage.getItem("cart") || "[]");
-		const existingItem = cartItems.find((item: any) => item.id === gameId);
-		setProductAdded(!!existingItem);
-	}, [gameId]);
+		if(typeof window !== "undefined"){
+			const card = JSON.parse(localStorage.getItem("cart") || '{"cartItems": [], "totalPrice": 0}');
+			const cartItems = card?.cartItems || [];
+			const existingItem = cartItems.find((item: any) => item.product.id === game.id);
+			setProductAdded(!!existingItem);
+			setCart(card);
+		}
+		//eslint-disable-next-line
+	}, []);
 
 	return (
 		<button className="game-card-button mt-2" onClick={productAdded ? removeFromCart : addToCart}>
