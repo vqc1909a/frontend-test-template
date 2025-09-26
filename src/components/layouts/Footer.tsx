8a0bf96483182cefa936578b3ab7645a3a1ab63f
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 17:16:13 -0500

    feat(layout): implement footer component with logo and link

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<div className="footer">
+			<div className="container-custom">
+        <Link href="/catalog" className="flex items-center">
+          <Image
+            src="/icons/Apply Digital Logo.svg"
+            alt="Apply Digital Logo"
+            width={170}
+            height={44}
+            className="footer-logo"
+          />
+        </Link>
+			</div>
+		</div>
+	);
+};
