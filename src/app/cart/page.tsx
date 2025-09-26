5fe5f10d57c5be6bda1c3c06f7b93fc22c15cbb0
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 17:51:45 -0500

    feat(cart): create Cart page with initial structure

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+import { GamesButton } from "@/components/pages/catalog/GamesButton";
+import { GamesList } from "@/components/pages/catalog/GamesList";
+import { GenreFilter } from "@/components/pages/catalog/GenreFilter";
+import { Suspense } from "react";
+import { GamesListSkeleton } from "../ui/skeletons";
+
+export default async function Cart() {
+
+	return (
+		<div className="container-custom py-8 xs:py-10 md:py-12 flex flex-col gap-12">
+			
+			
+		</div>
+	);
+}
