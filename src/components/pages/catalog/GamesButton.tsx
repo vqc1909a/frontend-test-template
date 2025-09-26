672bad11fddc747759552109c6a794e904179bcd
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 03:44:46 -0500

    feat(catalog): add loading state and skeleton component to GamesButton

--- src/components/pages/catalog/GamesButton.tsx
+++ src/components/pages/catalog/GamesButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GamesButtonSkeleton } from "@/app/ui/skeletons";
 import {getGames} from "@/services/getGames";
 import Link from "next/link";
 import {
@@ -28,6 +29,7 @@ export const GamesButton = ({genre, page}: GamesButtonProps) => {
 	const pathname = usePathname();
 	const searchParams = useSearchParams();
 
+  const [loading, setLoading] = useState(false);
 	const [totalPages, setTotalPages] = useState(1);
 	const [currentPage, setCurrentPage] = useState(1);
 
@@ -35,13 +37,19 @@ export const GamesButton = ({genre, page}: GamesButtonProps) => {
 
 	useEffect(() => {
 		const getGamesService = async () => {
+			setLoading(true);
 			const {totalPages, currentPage} = await getGames({genre, page});
 			setTotalPages(totalPages);
-      setCurrentPage(currentPage)
+      setCurrentPage(currentPage);
+			setLoading(false);
 		};
 
 		getGamesService();
 	}, [genre, page]);
+  
+  if(loading) {
+    return <GamesButtonSkeleton />
+  }
 
 	if (!hasMorePages) {
 		return null;
@@ -51,7 +59,7 @@ export const GamesButton = ({genre, page}: GamesButtonProps) => {
 		<div className="flex justify-start items-center">
 			<Link
 				href={createPageURL(currentPage + 1, pathname, searchParams)}
-				className="btn-primary"
+				className="btn-primary w-full md:w-auto"
 			>
 				See More
 			</Link>
