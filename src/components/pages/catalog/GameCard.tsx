012584f5f12d2905157cac8d2924494018a30b5d
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 10:22:10 -0500

    feat(catalog): add GameCard component to display game details

---
+++ src/components/pages/catalog/GameCard.tsx
@@ -0,0 +1,44 @@
+import { Game } from "@/services/getGames";
+import Image from "next/image";
+
+interface GameCardProps {
+  game: Game;
+}
+export const GameCard = ({game}: GameCardProps) => {
+  return (
+		<div className="game-card">
+			{/* Game Image */}
+			<div
+				className="relative overflow-hidden rounded-t-2xl"
+				style={{height: "240px"}}
+			>
+				<Image
+					src={game.image}
+					alt={game.name}
+					fill
+					sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+					className="object-cover hover:scale-105 transition-transform duration-300 will-change-transform"
+				/>
+			</div>
+
+			{/* Genre */}
+			<p className="text-custom-text-secondary text-base leading-4 font-bold uppercase tracking-normal">
+				{game.genre}
+			</p>
+
+			{/* Product Name and Price */}
+			<div className="flex items-center justify-between">
+				<h3 className="text-custom-text-primary font-bold text-lg leading-5 tracking-wide flex-1 mr-3 truncate">
+					{game.name}
+				</h3>
+				<span className="text-custom-text-primary font-bold text-xl leading-6 tracking-wide whitespace-nowrap">
+					${game.price}
+				</span>
+			</div>
+
+			{/* Add to Cart Button */}
+			<button className="game-card-button mt-2">Add to Cart</button>
+		</div>
+	);
+}
+
