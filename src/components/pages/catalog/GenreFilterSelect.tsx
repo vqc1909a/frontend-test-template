20d689f5b15081515e2b7827d5da619b8185613b
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-26 01:01:29 -0500

    feat(catalog): add defaultValue to GenreFilterSelect

--- src/components/pages/catalog/GenreFilterSelect.tsx
+++ src/components/pages/catalog/GenreFilterSelect.tsx
@@ -12,16 +12,16 @@ export const GenreFilterSelect = () => {
   const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const genre = event.target.value;
     const params = new URLSearchParams(searchParams);
-
     !!genre ? params.set("genre", genre) : params.delete("genre");
-
     replace(`${pathname}?${params.toString()}`);
   }
+
   return (
 		<select
 			id="genre-filter"
 			className="genre-select flex-auto text-xl leading-6 tracking-[0.4px] bg-custom-bg-primary border-none text-custom-text-primary px-2 py-2 rounded-md focus:outline-none cursor-pointer"
-      onChange={onChange}
+			onChange={onChange}
+			defaultValue={searchParams.get("genre") || ""}
 		>
 			<option value="">All</option>
 			{availableFilters.map((genre) => (
