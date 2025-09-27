aa284201d1b0dd3547c15268090143a833bb4899
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-27 00:48:24 -0500

    feat(cart): implement CartContent component structure

---
+++ src/components/pages/cart/CartContent.tsx
@@ -0,0 +1,16 @@
+
+export const CartContent = () => {
+  return (
+    <div className="flex flex-col lg:flex-row gap-10 lg:gap-14">
+				{/* Cart Products */}
+				<div className="flex-[3_3_0%]">
+					
+				</div>
+
+				{/* Cart Summary */}
+				<div className="flex-[2_2_0%]">
+					
+				</div>
+    </div>
+  );
+}
