e702ce7279f8f1ac51ebb7977fdf357df48fcaf2
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-27 18:00:49 -0500

    refactor(cart): replace useCart hook with CartContext in CartProducts and CartTitle components

--- src/components/pages/cart/CartProducts.tsx
+++ src/components/pages/cart/CartProducts.tsx
@@ -1,16 +1,17 @@
 'use client';
-import { useCart } from "@/utils/hooks/useCart";
+import { CartContext } from "@/utils/context/CartContext";
 import { CartProduct } from "./CartProduct";
+import {useContext} from "react";
 
 export const CartProducts = () => {
-	const { cartItems, removeFromCart } = useCart();
+	const {cartItems} = useContext(CartContext);
+
 	return (
 		<div className="flex flex-col gap-0">
 			{cartItems.map((item) => (
 				<CartProduct
 					key={item.product.id}
 					item={item.product}
-					removeFromCart={removeFromCart}
 				/>
 			))}
 		</div>
--- src/components/pages/cart/CartTitle.tsx
+++ src/components/pages/cart/CartTitle.tsx
@@ -1,13 +1,16 @@
-import { useCart } from "@/utils/hooks/useCart";
+'use client'
+import { CartContext } from "@/utils/context/CartContext";
+import { useContext } from "react";
 
 export const CartTitle = () => {
+	const { cartItems } = useContext(CartContext);
   return (
 		<div className="flex flex-col gap-3">
 			<h2 className="text-2xl leading-7 md:text-4xl md:leading-10 font-bold text-left tracking-[0.4px]">
 				Your Cart
 			</h2>
 			<p className="text-xl leading-6 md:text-2xl md:leading-7 tracking-[0.4px] font-normal">
-				3 items
+				{cartItems.length} items
 			</p>
 		</div>
 	);
