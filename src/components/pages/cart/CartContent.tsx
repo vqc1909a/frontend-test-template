3d0955a0d231f122d0edf8bbcb7bd587baf7b7cc
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-27 03:17:32 -0500

    feat(cart): integrate CartProducts and CartSummary components in CartContent

--- src/components/pages/cart/CartContent.tsx
+++ src/components/pages/cart/CartContent.tsx
@@ -1,15 +1,17 @@
+import { CartProducts } from "./CartProducts";
+import { CartSummary } from "./CartSummary";
 
 export const CartContent = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-10 lg:gap-14">
 				{/* Cart Products */}
 				<div className="flex-[3_3_0%]">
-					
+					<CartProducts />
 				</div>
 
 				{/* Cart Summary */}
 				<div className="flex-[2_2_0%]">
-					
+					<CartSummary />
 				</div>
     </div>
   );
