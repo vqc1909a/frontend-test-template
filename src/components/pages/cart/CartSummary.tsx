c82f1287ce4e357a28ced189f57af4fabcd58c00
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-28 00:26:28 -0500

    feat(cart): implement dynamic cart item display and total calculation

--- src/components/pages/cart/CartSummary.tsx
+++ src/components/pages/cart/CartSummary.tsx
@@ -1,6 +1,12 @@
+'use client';
+import { useContext } from "react";
+import { CartContext } from "@/utils/context/CartContext";
 import Link from "next/link";
+import { calculateItemTotal } from "@/utils/helpers/cart";
 
 export const CartSummary = () => {
+	const { cartItems, totalPrice } = useContext(CartContext);
+
 	return (
 		<div className="flex flex-col gap-8 mt-4 md:mt-0">
 			<div className="cart-summary">
@@ -10,35 +16,22 @@ export const CartSummary = () => {
 						Order Summary
 					</h2>
 					<p className="text-lg leading-6 tracking-wide font-normal">
-						3 items
+						{cartItems.length} items
 					</p>
 				</div>
 				{/* Order Items */}
+				
 				<div className="flex flex-col border-b border-custom-border-card py-6 gap-4">
-					<div className="flex items-center justify-between">
-						<h3 className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide flex-1 min-w-0 truncate">
-							Product Name
-						</h3>
-						<span className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide whitespace-nowrap flex-shrink-0">
-							$ 00.00
-						</span>
-					</div>
-					<div className="flex items-center justify-between">
-						<h3 className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide flex-1 min-w-0 truncate">
-							Product Name
-						</h3>
-						<span className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide whitespace-nowrap flex-shrink-0">
-							$ 00.00
-						</span>
-					</div>
-					<div className="flex items-center justify-between">
-						<h3 className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide flex-1 min-w-0 truncate">
-							Product Name
-						</h3>
-						<span className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide whitespace-nowrap flex-shrink-0">
-							$ 00.00
-						</span>
-					</div>
+					{cartItems.map(({product, quantity}) => (
+						<div key={product.id} className="flex items-center justify-between">
+							<h3 className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide flex-1 min-w-0 truncate">
+								{product.name}
+							</h3>
+							<span className="text-custom-text-primary font-normal text-lg leading-6 tracking-wide whitespace-nowrap flex-shrink-0">
+								${calculateItemTotal(product.price, quantity)}
+							</span>
+						</div>
+					))}
 				</div>
 				{/* Order Total */}
 				<div className="flex items-center justify-between mb-6">
@@ -46,7 +39,7 @@ export const CartSummary = () => {
 						Order Total
 					</h3>
 					<span className="text-custom-text-primary font-bold text-xl leading-6 tracking-wide whitespace-nowrap">
-						$ 000.00
+						$ {totalPrice}
 					</span>
 				</div>
 			</div>
