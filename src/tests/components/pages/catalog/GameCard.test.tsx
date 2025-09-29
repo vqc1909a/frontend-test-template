5d9ec5156aeefe2cad877c70d1690c0f2085d672
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-29 02:48:47 -0500

    test(catalog): add tests for GameCard component

---
+++ src/tests/components/pages/catalog/GameCard.test.tsx
@@ -0,0 +1,47 @@
+import {CartProvider} from "@/utils/context/CartProvider";
+import {allGames} from "@/utils/endpoint";
+import {cleanup, render, screen} from "@testing-library/react";
+import {afterEach, beforeEach, describe, expect, test, vi} from "vitest";
+import { GameCard } from "@/components/pages/catalog/GameCard";
+
+const mockAddToCart = vi.fn();
+const mockRemoveFromCart = vi.fn();
+const mockIsInCart = vi.fn(() => false);
+
+vi.mock("@/utils/hooks/useCart", async () => {
+	const originalModule = await vi.importActual("@/utils/hooks/useCart");
+	return {
+		...originalModule,
+		useCart: () => ({
+			addToCart: mockAddToCart,
+			removeFromCart: mockRemoveFromCart,
+			isInCart: mockIsInCart,
+		}),
+	};
+});
+
+describe("Tests on GameCard Component", () => {
+	beforeEach(() => {
+		localStorage.clear();
+		vi.clearAllMocks();
+	});
+
+	afterEach(() => {
+		cleanup();
+		vi.restoreAllMocks();
+	});
+
+	test("should render the card correctly", () => {
+		const game = allGames[0];
+		render(
+			<CartProvider>
+				<GameCard game={game} />
+			</CartProvider>
+		);
+		expect(screen.getByRole("img", {name: new RegExp(game.name, "i")})).toBeInTheDocument();
+		expect(screen.getByLabelText(/game-genre/i)).toHaveTextContent(game.genre);
+		expect(screen.getByRole("heading", {level: 3, name: new RegExp(game.name, "i")})).toBeInTheDocument();
+		expect(screen.getByLabelText(/game-price/i)).toHaveTextContent(`$${game.price}`);
+		expect(screen.getByRole("button", {name: /add to cart/i})).toBeInTheDocument();
+	});
+});
