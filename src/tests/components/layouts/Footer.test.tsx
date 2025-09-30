50ff9e74b903a3165b6c98ed0e8e917dcff747f7
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-30 00:32:24 -0500

    test(footer): add unit tests for Footer component layout

---
+++ src/tests/components/layouts/Footer.test.tsx
@@ -0,0 +1,26 @@
+import {cleanup, render, screen, within} from "@testing-library/react";
+import {afterEach, beforeEach, describe, expect, test, vi} from "vitest";
+import { Footer } from "@/components/layouts/Footer";
+
+
+describe("Tests on Header Layout", () => {
+	beforeEach(() => {
+		localStorage.clear();
+		vi.clearAllMocks();
+	});
+
+	afterEach(() => {
+		cleanup();
+		vi.restoreAllMocks();
+	});
+
+	const getHomeLink = () => screen.getByRole("link", {name: /apply digital logo/i});
+
+	test("should render the footer correctly", () => {
+		render(<Footer />);
+		screen.debug();
+		expect(getHomeLink()).toBeInTheDocument();
+		expect(getHomeLink()).toHaveAttribute("href", "/");
+		expect(within(getHomeLink()).getByRole("img", {name: /apply digital logo/i})).toBeInTheDocument();
+	});
+});
