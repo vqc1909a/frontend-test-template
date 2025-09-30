275b74579e9e768f0545b1b0d229a1c95d3db7e5
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-30 00:13:30 -0500

    test(header): add unit tests for Header component layout

---
+++ src/tests/components/layouts/Header.test.tsx
@@ -0,0 +1,29 @@
+import {cleanup, render, screen, within} from "@testing-library/react";
+import {afterEach, beforeEach, describe, expect, test, vi} from "vitest";
+import { Header } from "@/components/layouts/Header";
+
+
+describe("Tests on Header Layout", () => {
+	beforeEach(() => {
+		localStorage.clear();
+		vi.clearAllMocks();
+	});
+
+	afterEach(() => {
+		cleanup();
+		vi.restoreAllMocks();
+	});
+
+	const getHomeLink = () => screen.getByRole("link", {name: /gamershop/i});
+	const getCartLink = () => screen.getByRole("link", {name: /cart/i});
+
+	test("should render the header correctly", () => {
+		render(<Header />);
+		expect(getHomeLink()).toBeInTheDocument();
+		expect(getCartLink()).toBeInTheDocument();
+		expect(getHomeLink()).toHaveAttribute("href", "/catalog");
+		expect(getCartLink()).toHaveAttribute("href", "/cart");
+		expect(within(getHomeLink()).getByRole("heading", {level: 1, name: /gamershop/i})).toBeInTheDocument();
+		expect(within(getCartLink()).getByRole("img", {name: /cart/i})).toBeInTheDocument();
+	});
+});
