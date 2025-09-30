67f8fe368f562dc0e2d6eee0623e4522a30861e9
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-30 11:27:47 -0500

    feat: enhance user experience with custom error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -329,13 +329,13 @@ export default function Error({
 			</svg>
 
 			<div className="flex flex-col items-center justify-center">
-				<p className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-wider text-gray-600 mt-8">
+				<p className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-wider text-custom-text-primary mt-8">
 					500
 				</p>
-				<p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-600 mt-2">
+				<p className="text-2xl md:text-3xl lg:text-4xl font-bold text-custom-text-primary mt-2">
 					Server Error
 				</p>
-				<p className="md:text-lg xl:text-xl text-gray-500 mt-4">
+				<p className="md:text-lg xl:text-xl text-custom-text-secondary mt-4">
 					Whoops, something went wrong on our servers.
 				</p>
 			</div>
