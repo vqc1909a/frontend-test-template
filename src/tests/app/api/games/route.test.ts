a50ccd5a1a8b02a6de949174f58ec8d08b8dd0c2
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-09-30 03:20:24 -0500

    test(games): add unit tests for API route handling games

---
+++ src/tests/app/api/games/route.test.ts
@@ -0,0 +1,64 @@
+import {GET} from "@/app/api/games/route";
+import {cleanup} from "@testing-library/react";
+import {afterEach, beforeEach, describe, expect, test, vi} from "vitest";
+import {availableFilters} from "../../../../utils/endpoint";
+
+vi.mock("@/utils/endpoint", async () => {
+	const actual = await vi.importActual("@/utils/endpoint");
+	return {
+		...actual,
+		delay: vi.fn().mockResolvedValue(undefined),
+	};
+});
+
+describe("Tests on API Route: /api/games", () => {
+	beforeEach(() => {
+		localStorage.clear();
+		vi.clearAllMocks();
+	});
+
+	afterEach(() => {
+		cleanup();
+		vi.restoreAllMocks();
+	});
+
+	test("should return games with default parameters", async () => {
+		const request = new Request("http://localhost:3000/api/games");
+
+		const response = await GET(request);
+		const data = await response.json();
+		expect(response.status).toBe(200);
+
+		expect(data.games).toHaveLength(12);
+		expect(data.availableFilters).toEqual(availableFilters);
+		expect(data.currentPage).toBe(1);
+		expect(data.totalPages).toBe(3);
+	});
+	test("should return games filtered by genre", async () => {
+		const genre = "Action";
+		const request = new Request(
+			`http://localhost:3000/api/games?genre=${genre}`
+		);
+		const response = await GET(request);
+		const data = await response.json();
+		expect(response.status).toBe(200);
+		expect(data.games).toHaveLength(5);
+		expect(data.availableFilters).toEqual(availableFilters);
+		expect(data.currentPage).toBe(1);
+		expect(data.totalPages).toBe(1);
+	});
+
+	test("should return games for a specific page", async () => {
+		const page = 2;
+		const request = new Request(
+			`http://localhost:3000/api/games?page=${page}`
+		);
+		const response = await GET(request);
+		const data = await response.json();
+		expect(response.status).toBe(200);
+		expect(data.games).toHaveLength(12);
+		expect(data.availableFilters).toEqual(availableFilters);
+		expect(data.currentPage).toBe(page);
+		expect(data.totalPages).toBe(3);
+	});
+});
